2a3616ab897e453e04fccad392d29b8b92b6a1aa
Lola Barbier <lolabarbier@hotmail.com>
2017-12-04 16:04:28 +0100

    first migration ok

---
+++ db/migrate/20171204145208_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :status
+      t.integer :goal_amount
+      t.integer :saved_total_amount
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
