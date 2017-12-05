85594afbc451037862e85ad63509724f961d2e25
Lola Barbier <lolabarbier@hotmail.com>
2017-12-05 17:23:27 +0100

    everything works except destroy method, save before tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :user_projects, dependent: :destroy
+  has_many :user_projects
   has_many :projects, through: :user_projects
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
