d1271798660d754226f207145c5adc6b6ee16028
adrien moison <adrimoison@gmail.com>
2017-12-07 16:54:56 +0100

    cardv1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_authorized, only: :home
   def home
+    @home = "" #pour la navbar de /projects
   end
 
   def about
