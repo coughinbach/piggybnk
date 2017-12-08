71cdb3d1fe532253e27c65971c28210b7605ee65
adrien moison <adrimoison@gmail.com>
2017-12-08 15:07:32 +0100

    signup fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+  end
 
   private
 
