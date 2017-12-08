23a501e81038c38a74112d16102d9106e6e3ae9d
Jobrann Mous <jobrann.mous@hotmail.com>
2017-12-08 12:44:27 +0100

    scheduled heroku job for weekly charge every sunday at 00:30

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resource :creditcard, only: [:create]
   resource :payment, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
